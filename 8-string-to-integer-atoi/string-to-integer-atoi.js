a569c28d32181e142ab1ae050b6c5b6a3292ad1e
prajwalbhat39 <112185711+prajwalbhat39@users.noreply.github.com>
2026-07-27 17:27:08 +0530

    Time: 3 ms (25.09%) | Memory: 55.9 MB (90.47%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/string-to-integer-atoi.js
@@ -9,25 +9,25 @@ var myAtoi = function(s) {
     let result = ""
     let digitRead = false
     let signRead = false
-    for(let i =0 ; i< s.length ; i++){
-        if(s[i] === " " && digitRead === false && signRead === false){
+    for (let i = 0; i < s.length; i++) {
+
+        if (s[i] === " " && digitRead === false && signRead === false) {
             continue;
-        }else if(s[i] === " " && (digitRead === true || signRead === true)){
-            break;
-        }else if((s[i] === "-" || s[i] === "+")  && signRead === false &&  digitRead === false){
-            result +=  s[i]
+        }
+
+        if ((s[i] === "-" || s[i] === "+") && signRead === false && digitRead === false) {
+            result += s[i];
             signRead = true;
+            continue;
+        }
 
-        }else if((s[i] === "-" || s[i] === "+") &&  signRead === true && digitRead === true){
-            break;
-        }else if((s[i] === "-" || s[i] === "+") &&  signRead === true && digitRead === false){
-              break;
-        }else if(isDigit(s[i])){
+        if (isDigit(s[i])) {
+            result += s[i];
+            digitRead = true;
+            continue;
+        }
 
-            result += s[i]
-            digitRead =true;
-
-        }else if(!isDigit(s[i])){
+        else {
             break;
         }
     }
@@ -43,6 +43,5 @@ var myAtoi = function(s) {
     if (result > (2 ** 31) - 1) {
         return (2 ** 31) - 1;
     }
-    // console.log(result)
     return result
 };
