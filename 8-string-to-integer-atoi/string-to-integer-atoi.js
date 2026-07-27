070f52fa2784b6a0ee708baa473e391dcc0143b6
prajwalbhat39 <112185711+prajwalbhat39@users.noreply.github.com>
2026-07-27 17:35:28 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (95.41%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/string-to-integer-atoi.js
@@ -3,45 +3,46 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    function isDigit(char) {
+ function isDigit(char) {
         return char >= '0' && char <= '9';
     }
-    let result = ""
-    let digitRead = false
-    let signRead = false
-    for (let i = 0; i < s.length; i++) {
+  let result = 0;
+let sign = 1;
+let digitRead = false;
+let signRead = false;
 
-        if (s[i] === " " && digitRead === false && signRead === false) {
-            continue;
-        }
+for (let i = 0; i < s.length; i++) {
 
-        if ((s[i] === "-" || s[i] === "+") && signRead === false && digitRead === false) {
-            result += s[i];
-            signRead = true;
-            continue;
-        }
-
-        if (isDigit(s[i])) {
-            result += s[i];
-            digitRead = true;
-            continue;
-        }
-
-        else {
-            break;
-        }
-    }
-    if(result === "-" || result === "+"){
-        return 0;
-    }
-    result = Number(result);
-
-    if (result < -(2 ** 31)) {
-        return -(2 ** 31);
+    if (s[i] === " " && digitRead === false && signRead === false) {
+        continue;
     }
 
-    if (result > (2 ** 31) - 1) {
-        return (2 ** 31) - 1;
+    if ((s[i] === "-" || s[i] === "+") && signRead === false && digitRead === false) {
+        signRead = true;
+        if (s[i] === "-") {
+            sign = -1;
+        }
+        continue;
     }
-    return result
+
+    if (isDigit(s[i])) {
+        digitRead = true;
+        result = result * 10 + (s[i] - '0');
+        continue;
+    }
+
+    break;
+}
+
+result *= sign;
+
+if (result < -(2 ** 31)) {
+    return -(2 ** 31);
+}
+
+if (result > (2 ** 31) - 1) {
+    return (2 ** 31) - 1;
+}
+
+return result;
 };
