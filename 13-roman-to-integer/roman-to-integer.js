8229f556e074a0337f695a7e072c5e6b6c9239f4
prajwalbhat39 <112185711+prajwalbhat39@users.noreply.github.com>
2026-07-30 16:50:13 +0530

    Time: 8 ms (23.43%) | Memory: 62.3 MB (16.89%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -4,64 +4,76 @@
  */
 var romanToInt = function(s) {
     let i = 0;
-    let totalSum =0;
+    let totalSum = 0;
+
     const romanMap = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-};
-    while(i< s.length){
-        if(s[i] === "I"){
-            if(s[i+1] === "V"){
-                totalSum += 4;
-                i+=2;
-            }
-            else if(s[i+1] === "X"){
-                totalSum +=9;
-                i+=2;
-            }
-            else{
-                totalSum+=1;
-                i+=1
-            }
-        }else if(s[i] === "X"){
-            if(s[i+1] === "L"){
-                totalSum += 40;
-                i+=2;
-            }
-            else if(s[i+1] === "C"){
-                totalSum +=90;
-                i+=2;
-            }
-            else{
-                totalSum+=10;
-                i+=1
-            }
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+        IV: 4,
+        IX: 9,
+        XL: 40,
+        XC: 90,
+        CD: 400,
+        CM: 900
+    };
 
-        }else if(s[i] === "C"){
-            if(s[i+1] === "D"){
-                totalSum += 400;
-                i+=2;
+    while (i < s.length) {
+
+        if (s[i] === "I") {
+            if (s[i + 1] === "V") {
+                totalSum += romanMap["IV"];
+                i += 2;
             }
-            else if(s[i+1] === "M"){
-                totalSum +=900;
-                i+=2;
+            else if (s[i + 1] === "X") {
+                totalSum += romanMap["IX"];
+                i += 2;
             }
-            else{
-                totalSum+=100;
-                i+=1
+            else {
+                totalSum += romanMap[s[i]];
+                i += 1;
             }
-        }else{
+        }
+
+        else if (s[i] === "X") {
+            if (s[i + 1] === "L") {
+                totalSum += romanMap["XL"];
+                i += 2;
+            }
+            else if (s[i + 1] === "C") {
+                totalSum += romanMap["XC"];
+                i += 2;
+            }
+            else {
+                totalSum += romanMap[s[i]];
+                i += 1;
+            }
+        }
+
+        else if (s[i] === "C") {
+            if (s[i + 1] === "D") {
+                totalSum += romanMap["CD"];
+                i += 2;
+            }
+            else if (s[i + 1] === "M") {
+                totalSum += romanMap["CM"];
+                i += 2;
+            }
+            else {
+                totalSum += romanMap[s[i]];
+                i += 1;
+            }
+        }
+
+        else {
             totalSum += romanMap[s[i]];
-            i+=1
+            i += 1;
         }
+    }
 
-
-        }
-        return totalSum
-    
+    return totalSum;
 };
