cbecfdcfdb2384014de691c3ee50adc8d84d2c91
prajwalbhat39 <112185711+prajwalbhat39@users.noreply.github.com>
2026-08-01 12:38:04 +0530

    Time: 2 ms (85.56%) | Memory: 62.4 MB (12.94%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.js
@@ -0,0 +1,85 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let carry = 0; 
+    let finResult = []
+    let r =0;
+    let answer = 0;
+    let node= null;
+    while((l2 && l2.next != null) || (l1 && l1.next != null)){
+        if(l1 && l2){
+            r = l1.val + l2.val + carry;
+            answer = r % 10;
+            carry = Math.floor(r / 10);
+            node =  new ListNode(answer , null)
+            finResult.push(node);
+            l1= l1.next;
+            l2 = l2.next;
+        }else if(l1 && !l2){
+            r = l1.val + carry;
+            answer = r % 10;
+            carry =  Math.floor(r / 10);
+            node =  new ListNode(answer , null)
+            finResult.push(node);
+            l1 = l1.next;
+        }else if(l2 && !l1){
+            r = l2.val + carry;
+            answer = r % 10;
+            carry =  Math.floor(r / 10);
+            node =  new ListNode(answer , null)
+            finResult.push(node);
+            l2 = l2.next;
+        }
+        // console.log(finResult);
+    }
+    if(l1 && l2){
+        r = l1.val + l2.val + carry;
+        answer = r % 10;
+        carry =  Math.floor(r / 10);
+        node =  new ListNode(answer , null);
+        finResult.push(node);
+        if(carry > 0){
+            node =  new ListNode(carry , null);
+            finResult.push(node);
+        }
+       
+    }else if(l1 && !l2){
+        r = l1.val + carry;
+        answer = r % 10;
+        carry =  Math.floor(r / 10);
+        node =  new ListNode(answer , null);
+        finResult.push(node);
+        if(carry > 0){
+            node =  new ListNode(carry , null);
+            finResult.push(node);
+        }
+    }else if(l2 && !l1){
+        r = l2.val + carry;
+        answer = r % 10;
+        carry =  Math.floor(r / 10);
+        node =  new ListNode(answer , null);
+        finResult.push(node);
+
+        if(carry > 0){
+            node =  new ListNode(carry , null);
+            finResult.push(node);
+        }
+       
+    }
+    // console.log(finResult)
+    let curr = finResult[0]
+    for(let i = 0 ; i< finResult.length-1 ; i++){
+        finResult[i].next = finResult[i+1]
+    }
+    return curr;
+};
