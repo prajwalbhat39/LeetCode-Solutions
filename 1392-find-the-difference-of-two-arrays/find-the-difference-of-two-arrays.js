aacdaa41abdd5edff8d4b5cbc9dcf75a623ad2be
Prajwal V Bhat <112185711+prajwalbhat39@users.noreply.github.com>
2026-08-13 20:59:22 +0530

    Time: 6 ms (94.78%) | Memory: 63.2 MB (7.50%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
@@ -36,14 +36,29 @@ var findDifference = function(nums1, nums2) {
     //     }
     // }
 
-    for(let element of nums1){
-        if(!nums2.includes(element) && !result[0].includes(element)){
-            result[0].push(element)
+    // for(let element of nums1){
+    //     if(!nums2.includes(element) && !result[0].includes(element)){
+    //         result[0].push(element)
+    //     }
+    // }
+    // for(let element of nums2){
+    //     if(!nums1.includes(element) && !result[1].includes(element)){
+    //         result[1].push(element)
+    //     }
+    // }
+
+    let set1 =  new Set(nums1);
+    let set2 = new Set(nums2);
+
+    for(let element of set1){
+        if(!set2.has(element)){
+            result[0].push(element);
         }
     }
-    for(let element of nums2){
-        if(!nums1.includes(element) && !result[1].includes(element)){
-            result[1].push(element)
+    
+    for(let element of set2){
+        if(!set1.has(element)){
+            result[1].push(element);
         }
     }
     
