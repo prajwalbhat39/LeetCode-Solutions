4756b446f4aa9381b40c22df9a4d2d45229b1798
Prajwal V Bhat <112185711+prajwalbhat39@users.noreply.github.com>
2026-08-13 21:20:19 +0530

    Time: 17 ms (23.33%) | Memory: 61.7 MB (57.26%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
@@ -1,68 +1,68 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[][]}
- */
 var findDifference = function(nums1, nums2) {
-    // let map1 = new Map();
-    // let map2 = new Map();
 
-    // for (let element of nums1) {
-    //     if (map1.has(element)) {
-    //         map1.set(element, map1.get(element) + 1);
-    //     } else {
-    //         map1.set(element, 1);
-    //     }
-    // }
-
-    // for (let element of nums2) {
-    //     if (map2.has(element)) {
-    //         map2.set(element, map2.get(element) + 1);
-    //     } else {
-    //         map2.set(element, 1);
-    //     }
-    // }
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
 
     let result = [[], []];
-    // for (let element of nums1) {
-    //     if (!map2.has(element) && !result[0].includes(element)) {
-    //         result[0].push(element);
-    //     }
-    // }
 
-    // for (let element of nums2) {
-    //     if (!map1.has(element) && !result[1].includes(element)) {
-    //         result[1].push(element);
-    //     }
-    // }
+    let i = 0;
+    let j = 0;
 
-    // for(let element of nums1){
-    //     if(!nums2.includes(element) && !result[0].includes(element)){
-    //         result[0].push(element)
-    //     }
-    // }
-    // for(let element of nums2){
-    //     if(!nums1.includes(element) && !result[1].includes(element)){
-    //         result[1].push(element)
-    //     }
-    // }
+    while (i < nums1.length || j < nums2.length) {
 
-    let set1 =  new Set(nums1);
-    let set2 = new Set(nums2);
+        // nums2 exhausted
+        if (j >= nums2.length) {
+            result[0].push(nums1[i]);
 
-    for(let element of set1){
-        if(!set2.has(element)){
-            result[0].push(element);
+            let value = nums1[i];
+            while (i < nums1.length && nums1[i] === value) {
+                i++;
+            }
+        }
+
+        // nums1 exhausted
+        else if (i >= nums1.length) {
+            result[1].push(nums2[j]);
+
+            let value = nums2[j];
+            while (j < nums2.length && nums2[j] === value) {
+                j++;
+            }
+        }
+
+        // nums1 value is smaller
+        else if (nums1[i] < nums2[j]) {
+            result[0].push(nums1[i]);
+
+            let value = nums1[i];
+            while (i < nums1.length && nums1[i] === value) {
+                i++;
+            }
+        }
+
+        // nums2 value is smaller
+        else if (nums2[j] < nums1[i]) {
+            result[1].push(nums2[j]);
+
+            let value = nums2[j];
+            while (j < nums2.length && nums2[j] === value) {
+                j++;
+            }
+        }
+
+        // Equal
+        else {
+            let value = nums1[i];
+
+            while (i < nums1.length && nums1[i] === value) {
+                i++;
+            }
+
+            while (j < nums2.length && nums2[j] === value) {
+                j++;
+            }
         }
     }
-    
-    for(let element of set2){
-        if(!set1.has(element)){
-            result[1].push(element);
-        }
-    }
-    
-    
 
     return result;
 };
