6583ae6fba0c89796455898e0300ff7a8c59deb8
Prajwal V Bhat <112185711+prajwalbhat39@users.noreply.github.com>
2026-08-13 20:53:24 +0530

    Time: 65 ms (5.06%) | Memory: 61.2 MB (64.60%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
@@ -4,37 +4,50 @@
  * @return {number[][]}
  */
 var findDifference = function(nums1, nums2) {
-    let map1 = new Map();
-    let map2 = new Map();
+    // let map1 = new Map();
+    // let map2 = new Map();
 
-    for (let element of nums1) {
-        if (map1.has(element)) {
-            map1.set(element, map1.get(element) + 1);
-        } else {
-            map1.set(element, 1);
-        }
-    }
+    // for (let element of nums1) {
+    //     if (map1.has(element)) {
+    //         map1.set(element, map1.get(element) + 1);
+    //     } else {
+    //         map1.set(element, 1);
+    //     }
+    // }
 
-    for (let element of nums2) {
-        if (map2.has(element)) {
-            map2.set(element, map2.get(element) + 1);
-        } else {
-            map2.set(element, 1);
-        }
-    }
+    // for (let element of nums2) {
+    //     if (map2.has(element)) {
+    //         map2.set(element, map2.get(element) + 1);
+    //     } else {
+    //         map2.set(element, 1);
+    //     }
+    // }
 
     let result = [[], []];
-    for (let element of nums1) {
-        if (!map2.has(element) && !result[0].includes(element)) {
-            result[0].push(element);
-        }
-    }
+    // for (let element of nums1) {
+    //     if (!map2.has(element) && !result[0].includes(element)) {
+    //         result[0].push(element);
+    //     }
+    // }
 
-    for (let element of nums2) {
-        if (!map1.has(element) && !result[1].includes(element)) {
-            result[1].push(element);
+    // for (let element of nums2) {
+    //     if (!map1.has(element) && !result[1].includes(element)) {
+    //         result[1].push(element);
+    //     }
+    // }
+
+    for(let element of nums1){
+        if(!nums2.includes(element) && !result[0].includes(element)){
+            result[0].push(element)
         }
     }
+    for(let element of nums2){
+        if(!nums1.includes(element) && !result[1].includes(element)){
+            result[1].push(element)
+        }
+    }
+    
+    
 
     return result;
 };
