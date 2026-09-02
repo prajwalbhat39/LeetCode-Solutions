876dbb9eb86fe09a84100575b7b11149d88ff01c
Prajwal V Bhat <112185711+prajwalbhat39@users.noreply.github.com>
2026-09-02 23:05:34 +0530

    Time: 5 ms (72.34%) | Memory: 56.2 MB (55.23%) - LeetSync

---
+++ 139-word-break/word-break.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    if(wordDict.includes(s)){
+        return true;
+    }
+    
+    dpArray = new Array(s.length+1).fill(false);
+    dpArray[0] =true;
+
+    for(let i = 0; i<s.length ; i++){
+        for(let j= 0 ; j<= i ; j++){
+            if(wordDict.includes(s.substring(j, i+1)) && dpArray[j] === true){
+                dpArray[i+1] = true;
+            }
+
+        }
+    }
+    return dpArray[s.length]
+
+};
