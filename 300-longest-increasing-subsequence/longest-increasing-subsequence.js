3f78cdcd82b352ea1632b4fcad15011398de97af
Prajwal V Bhat <112185711+prajwalbhat39@users.noreply.github.com>
2026-09-05 13:02:37 +0530

    Time: 5 ms (86.41%) | Memory: 56.9 MB (26.53%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.js
@@ -3,20 +3,27 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
-    let dp = new Array(nums.length).fill(0)
-    dp[0] = 1 ;
-    let maxi =1;
-    for(let i = 1 ; i<nums.length ; i++){
-         let j= i-1 ;
-         let tempMax = 0;
-         while(j >= 0){
-            if(nums[j] < nums[i]){
-                tempMax= Math.max(tempMax , dp[j])
+
+    let tails = [];
+
+    for (let num of nums) {
+
+        let left = 0;
+        let right = tails.length;
+
+        // Find first element >= num
+        while (left < right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if (tails[mid] < num) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
-            j--;
-         }
-        dp[i]= tempMax+1
-        maxi = Math.max(dp[i], maxi);
+        }
+
+        tails[left] = num;
     }
-    return maxi
+
+    return tails.length;
 };
